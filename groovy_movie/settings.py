ba1e6884ed270bd0c2b4119cf563f917bcfd9156
Gilad Weinberger <giladwe21@gmail.com>
2023-11-26 22:05:36 +0200

    .

--- groovy_movie/settings.py
+++ groovy_movie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)v*8w2ouj(30(%r$z4t55l6vroaaklm^-p#j%%w3s90&%3u(@_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
